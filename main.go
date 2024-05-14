2e993efbbf357842898044a096d9b3af1a5c1b41
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-14 18:07:47 +0600

    Update data.csv to contain only "Name" column and modify scraping to write only "Name" column to the CSV file
    The changes include:
    - Updating data.csv to only contain the "Name" column header

--- main.go
+++ main.go
@@ -20,16 +20,25 @@ func main() {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 	// write csv header
-	writer.Write([]string{"Name", "Symbol", "Market Cap (USD)", "Price (USD)", "Circulating Supply (USD)", "Volume (24h)", "Change (1h)", "Change (24h)", "Change (7d)"})
+	writer.Write([]string{"Name"})
 
 	// initialize colly
-	c := colly.NewCollector(colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"))
+	c := colly.NewCollector()
 
-	c.OnHTML("tbody tr ", func(e *colly.HTMLElement) {
-		writer.Write([]string{
-			e.ChildText(".cmc-link"),
-		})
+	c.OnHTML("tbody  ", func(e *colly.HTMLElement) {
+		result := e.ChildText(".cmc-table__column-name--name")
+		if result == "" {
+			fmt.Println("No result found for .cmc-table__column-name--name")
+		} else {
+			fmt.Println(result)
+			writer.Write([]string{result})
+		}
 	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
 	c.Visit("https://coinmarketcap.com/all/views/all/")
 
 	log.Printf("Scraping finished, check file %q for results\n", fName)
